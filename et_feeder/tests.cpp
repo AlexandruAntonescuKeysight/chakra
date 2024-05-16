0de5585aaf144915474e8abd8c12ffb169ae40a2
AlexandruAntonescuKeysight <alexandru.antonescu@keysight.com>
2024-05-16 05:31:10 +0300

    fixes include paths

--- et_feeder/tests.cpp
+++ et_feeder/tests.cpp
@@ -64,32 +64,21 @@ TEST_F(ETFeederTest, ConstructorTestETFeeder) {
 }
 
 TEST_F(ETFeederTest, RemoveTest) {
+    SetUp("et_feeder/chakra.0.et");
+    std::shared_ptr<Chakra::ETFeederNode> node = trace->lookupNode(216);
+    ASSERT_EQ(node->id(), 216);
+    trace->removeNode(216);
+    node = trace->lookupNode(216);
+    // ASSERT_EQ(node->id(), 5);
+}
+
+TEST_F(ETFeederTest, RemoveTestAndGetNext) {
     SetUp("et_feeder/llama_chakra.3.et");
-    // std::shared_ptr<Chakra::ETFeederNode> node;
-    std::shared_ptr<Chakra::ETFeederNode> node = trace->lookupNode(17);
-    std::cout << node->is_cpu_op() << " " << "\n";
-    // trace->removeNode(5);
-    // std::cout << "aiciii " << "\n";
-    // trace->removeNode(432);
-    // node = trace->lookupNode(216);
-    // node = trace->getNextIssuableNode();
-    // std::cout << node->id() << " " << "\n";
-    // std::cout << "aiciii " << "\n";
-    // node = trace->getNextIssuableNode();
-    // std::cout << node->id() << " " << "\n";
-    // node = trace->getNextIssuableNode();
-    // node = trace->getNextIssuableNode();
-    // std::cout << "aiciii " << "\n";
-    // node = trace->getNextIssuableNode();
-    // trace->removeNode(node->id());
-    // trace->removeNode(node->id());
-    // std::cout << node->id() << " " << "\n";
-    // trace->removeNode(node->id());
-    // trace->removeNode(node->id());
-    // std::vector<std::shared_ptr<Chakra::ETFeederNode>> children = node->getChildren(); 
-    // ASSERT_EQ(children[0]->id(), 217);
-    // ASSERT_EQ(children[1]->id(), 430);
-    // ASSERT_EQ(children[2]->id(), 435);
+    std::shared_ptr<Chakra::ETFeederNode> node = trace->lookupNode(5);
+    ASSERT_EQ(node->id(), 5);
+    trace->removeNode(5);
+    node = trace->getNextIssuableNode();
+    ASSERT_EQ(node->id(), 5);
 }
 
 // Add more test cases as needed
